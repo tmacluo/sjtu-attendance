660e03efb3b4d4c3a260f9a48c718fc4c50f234c
tmac.luo <tmac.luo@farfetch.com>
2023-05-14 09:25:53 +0800

    avoid duplicated checkin

--- src/main/java/com/sjtu/checkin/service/impl/CheckinServiceImpl.java
+++ src/main/java/com/sjtu/checkin/service/impl/CheckinServiceImpl.java
@@ -9,11 +9,13 @@ import com.sjtu.checkin.service.CheckinService;
 import com.sjtu.checkin.model.Checkin;
 import com.sjtu.checkin.service.BeaconService;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.time.Duration;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -22,6 +24,9 @@ import java.util.Optional;
 @Service
 @Slf4j
 public class CheckinServiceImpl implements CheckinService {
+
+    private static final long checkinDurationInSeconds = 120;
+
     public static final String EMPTY = "";
     @Autowired
     private CheckinMapper checkinMapper;
@@ -45,7 +50,7 @@ public class CheckinServiceImpl implements CheckinService {
 
     @Override
     public Checkin save(Checkin checkin) throws Exception {
-        if (StringUtils.hasLength(checkin.getStudentNo())){
+        if (StringUtils.hasLength(checkin.getStudentNo())) {
             boolean isMatched = checkin.getStudentNo().matches("^\\d{12}$");
             if (!isMatched) {
                 throw new Exception("学号不符合规范");
@@ -54,11 +59,38 @@ public class CheckinServiceImpl implements CheckinService {
 
         List<Beacon> availableBeacons = beaconService.getBeacons();
         checkin.setClassroom(determineClassroom(availableBeacons, checkin.getBeacons()));
+
+        avoidDuplicatedCheckin(checkin);
+
         checkin.setCreatedTime(ZonedDateTime.now(ZoneOffset.UTC));
         checkin.setLastUpdatedTime(ZonedDateTime.now(ZoneOffset.UTC));
         encodeBeacon(checkin);
         checkinMapper.saveCheckins(checkin);
         return checkin;
+    }
+
+    private void avoidDuplicatedCheckin(Checkin checkin) throws Exception {
+        List<Checkin> checkinHistory = getCheckinsWithFilter(checkin);
+        if (!CollectionUtils.isEmpty(checkinHistory)) {
+            val lastCheckin = checkinHistory.stream()
+                    .filter(history -> StringUtils.hasLength(history.getClassroom()))
+                    .max((o1, o2) -> {
+                        val createdTime1 = o1.getCreatedTime();
+                        val createdTime2 = o2.getCreatedTime();
+                        if (createdTime1.isEqual(createdTime2)) {
+                            return 0;
+                        }
+                        return o1.getCreatedTime().isAfter(o2.getCreatedTime()) ? 1 : -1;
+                    });
+
+            if (lastCheckin.isPresent()) {
+                val seconds = Duration.between(lastCheckin.get().getCreatedTime().withZoneSameInstant(ZoneOffset.UTC),
+                        ZonedDateTime.now().withZoneSameInstant(ZoneOffset.UTC)).getSeconds();
+                if (seconds <= checkinDurationInSeconds) {
+                    throw new Exception("你已签到成功，请2分钟后再试");
+                }
+            }
+        }
     }
 
     private void decodeBeacon(Checkin checkin) {
@@ -90,7 +122,7 @@ public class CheckinServiceImpl implements CheckinService {
             log.info("found the beacon with strongest rssi signal {}", strongestBeacon.get());
             Optional<Beacon> matchedBeacon = availableBeacons.stream()
                     // the name passed from frontend is UUID of that beacon with dash delimiter
-                    .filter(beacon -> beacon.getId().equalsIgnoreCase(strongestBeacon.get().getBeaconName().replaceAll("-","")))
+                    .filter(beacon -> beacon.getId().equalsIgnoreCase(strongestBeacon.get().getBeaconName().replaceAll("-", "")))
                     .findFirst();
             if (matchedBeacon.isPresent()) {
                 log.info("found matched classroom {}", matchedBeacon.get().getClassroom());
@@ -101,9 +133,6 @@ public class CheckinServiceImpl implements CheckinService {
     }
 
     private static Optional<Checkin.Beacon> getBeaconWithStrongestSignal(List<Checkin.Beacon> checkinBeacons) {
-
-        //FDA50693-A4E2-4FB1-AFCF-C6EB07647802
-
 
         return checkinBeacons.stream().max((o1, o2) -> {
             int rssi1 = Integer.parseInt(o1.getRssi());
